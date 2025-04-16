d41701f01da74ec67519f14f0b64173b68c30ed4
julia <julia>
2025-04-16 15:37:52 +0300

    all

---
+++ src/tests/tests.h
@@ -0,0 +1,21 @@
+#ifndef __TESTS_H__
+#define __TESTS_H__
+
+#include <check.h>
+#include <math.h>
+#include <stdio.h>
+
+#include "../build/main.h"
+
+Suite *s21_test_spin(void);
+// Suite *s21_test_eq_matrix(void);
+// Suite *s21_test_sum_matrix(void);
+// Suite *s21_test_sub_matrix(void);
+// Suite *s21_test_mult_number(void);
+// Suite *s21_test_mult_matrix(void);
+// Suite *s21_test_transpose(void);
+// Suite *s21_test_calc_complements(void);
+// Suite *s21_test_determinant(void);
+// Suite *s21_test_inverse_matrix(void);
+
+#endif
